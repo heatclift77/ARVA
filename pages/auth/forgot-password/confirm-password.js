37a7dfabb0eb0af3452e1fd8cccfe29d50268641
abudzr <abudzaralghifari8@gmail.com>
2021-05-05 22:51:54 +0700

    added authentication

--- pages/auth/forgot-password/confirm-password.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function konfirmasi_password() {
-    return (
-        <div>
-            <h1>konfirmasi pass</h1>
-        </div>
-    )
-}
--- pages/auth/forgot-password/reset-password.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Reset_password() {
-    return (
-        <div>
-            <h1>reset pass</h1>
-        </div>
-    )
-}
